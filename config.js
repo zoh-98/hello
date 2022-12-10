9482629d767825091482e4e1fb4307cb41b79c5a
zoh-98 <106838880+zoh-98@users.noreply.github.com>
2022-12-10 01:47:32 +0000

    Update config.js

--- config.js
+++ config.js
@@ -14,26 +14,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['21215924457'] //ur owner number
+global.ownername = "GRY KJ" //ur owner name
+global.ytname = "YT: 6JFL" //ur yt chanel name
+global.socialm = "GitHub: GRY KJ" //ur github or insta name
+global.location = "MOROCCO 3-1 PORTUGAL" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213'] //ur number
-global.ownernomer = "916909137213" //ur number
-global.ownertag = '916909137213' //ur tag number
-global.premium = ['916909137213'] //ur premium number
-global.botname = 'Cheems Bot V5' //ur bot name
-global.ownername = "🦄Dream Guy Xeon"
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD5' //script link
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "Cheems Bot" //ur sticker watermark author
-global.wm = "Xeon Bot Inc." //ur watermark
+global.owner = ['21215924457'] //ur number
+global.ownernomer = "21215924457" //ur number
+global.ownertag = '21215924457' //ur tag number
+global.premium = ['21215924457'] //ur premium number
+global.botname = 'GRY Bot V99' //ur bot name
+global.ownername = "🦄Dream Gry KJ"
+global.linkz = "HHHHHHHHHHHHHHHHHHHHHHHHHH" //your theme url which will be displayed on whatsapp
+global.websitex = "HHHHHHHHHHHHHHHHHH" //ur website to be displayed
+global.botscript = 'https://github.com/ZOH-98/CheemsBot-MD5' //script link
+global.themeemoji = "💛" //ur theme emoji
+global.packname = "Sticker By GRY KJ" //ur sticker watermark packname
+global.author = "GRY Bot" //ur sticker watermark author
+global.wm = "LOVE YOU ALL." //ur watermark
 
 // Other
 global.sessionName = 'session'
